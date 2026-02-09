1a1ff24deee6670c854c97e3ac6ea97195590ee6
jacobProvencherUL <japro72@ulaval.ca>
2026-02-08 19:44:22 -0500

    Worked a little bit on board representation

--- tests.py
+++ tests.py
@@ -1,15 +1,6 @@
 
 from itertools import batched
 
-
-decimal_number = 4_398_066_094_081
-print(hex(decimal_number))
-number = bin(decimal_number)[2:][::-1]
-
-array = list(batched(list(batched(number, 4)), 2))
-
-print(array)
-
 # todo : work on keyboard shortcuts setup
 # todo : go from decimal_number to binary number represent as check board
 
@@ -31,6 +22,22 @@ Ouput:
 0000 0000
 0001
 
+0b 1000 0000 0000 0000 0010 0101 0101 1010 0000 0000 0010 0000 0000 0000 0000 0000 0000 0000 0000
+
 """
+
+
+decimal_number = 4_398_066_094_081
+num = bin(decimal_number)[2:][::-1]
+
+def convert_integer_to_board_representation(number: int) -> str:
+    return bin(number)
+
+print(convert_integer_to_board_representation(decimal_number))
+
+
+
+
+
 
 
